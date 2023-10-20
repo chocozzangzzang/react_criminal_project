0878eee94484ed38c498052226b3a588254d69a4
chocozzang <jangjh_05@naver.com>
2023-10-21 03:06:05 +0900

    react criminal project 1st

---
+++ src/contents/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function main() {
+  return (
+    <div>
+        <iframe className="my-20"
+            src="https://public.tableau.com/views/total_16963303018100/sheet39?:language=ko-KR&publish=yes&:display_count=n&:origin=viz_share_link?:showVizHome=no?:embed=true"
+            title="main_dashboard"
+            width="1000"
+            height="700">
+        </iframe>
+    </div>
+  )
+}
+
+export default main;
